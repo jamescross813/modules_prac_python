fad2fd94759c8d4e6018cef16c6260e73e80b7eb
jamescross813 <crossjames813@gmail.com>
2023-03-21 22:46:44 -0500

    random.choice() used

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import random
 
 random_one = random.randint(1,100)
 random_list =[random.randint(1,100) for i in range(10)]
-
+random_two = random.choice(random_list)
 print(random_one)
 print(random_list)
+print(random_two)
